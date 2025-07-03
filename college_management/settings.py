f5a08837058e016b7c3b3343efd776357ba53d67
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-07-03 20:29:17 +0530

    Update settings.py

--- college_management/settings.py
+++ college_management/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://college-management-tugk.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
