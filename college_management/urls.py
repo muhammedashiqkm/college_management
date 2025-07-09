f6518afd647a3d08cf002d32a505da3b13789d05
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-07-09 23:43:01 +0530

    Update urls.py

--- college_management/urls.py
+++ college_management/urls.py
@@ -6,7 +6,7 @@ from core.views import college_user_panel
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/login/')),  # ✅ Redirect / to login
+    path('', RedirectView.as_view(url='/login/')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('panel/', college_user_panel, name='college-panel'),
